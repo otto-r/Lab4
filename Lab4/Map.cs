f2c6f9a927ab9dbb34f434747a1b4d7acfa8ef65
Otto <otto.rinaldo@gmail.com>
2017-09-22 15:33:56 +0200

    Added dynamic map

---
+++ Lab4/Map.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lab4
+{
+    class Map
+    {
+        //public Object[,] MapReader(Object[,])
+        //{
+        //    char[,] MapArray = new char[6, 22];
+        //    StreamReader MapReader = new StreamReader("Map.txt");
+
+        //    String MapString = MapReader.ReadToEnd();
+        //    String MapString2 = MapString.Replace(Environment.NewLine, "");
+
+        //    int counter = 0;
+        //    for (int i = 0; i < 6; i++)
+        //    {
+        //        for (int j = 0; j < 22; j++)
+        //        {
+        //            if (MapString2[counter] == '#')
+        //            {
+        //                Map[i, j] = MapString2[counter];
+        //            }
+        //            counter++;
+        //        }
+        //    }
+        //    return Map
+        //    //for (int i = 0; i < 6; i++)
+        //    //{
+        //    //    for (int j = 0; j < 22; j++)
+        //    //    {
+        //    //        Console.Write(MapArray[i, j]);
+        //    //    }
+        //    //    Console.WriteLine();
+        //    //}
+
+        //}
+    }
+}
--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Lab4
 {
@@ -14,21 +15,47 @@ namespace Lab4
             Player player = new Player();
             Monster monster1 = new Monster();
           
-            Object[,] map = new Object[,] { { new Wall(), new Wall(), new Wall(), new Wall(), new Wall() },
-                { new Wall(), new Floor(), new Floor(), new Floor(), new Wall() },
-                { new Wall(), new Floor(), new Floor(), new Floor(), new Wall() },
-                { new Wall(), new Floor(), new Floor(), new Floor(), new Wall() },
-                { new Wall(), new Wall(), new Wall(), new Wall(), new Wall() }};
-            
+            Object[,] map = new Object[6,22];
+            StreamReader MapReader = new StreamReader("Map.txt");
+
+            String MapString = MapReader.ReadToEnd();
+            String MapString2 = MapString.Replace(Environment.NewLine, "");
+
+            int counter = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 22; j++)
+                {
+                    if (MapString2[counter] == '#')
+                    {
+                        map[i, j] = new Wall();
+                    }
+                    else if (MapString2[counter] == 'D')
+                    {
+                        map[i, j] = new Door();
+                    }
+                    else if (MapString2[counter] == '-')
+
+                    {
+                        map[i, j] = new Floor();
+                    }
+                    //else if (MapString2[counter] == '')
+                    //{
+                    //    map[i, j] = new Floor();
+                    //}
+                    counter++;
+                }
+            }
+
             map[2, 2] = player;
             map[3, 1] = monster1;
 
             char input;
             while (true)
             {
-                for (int i = 0; i < 5; i++)
+                for (int i = 0; i < 6; i++)
                 {
-                    for (int j = 0; j < 5; j++)
+                    for (int j = 0; j < 22; j++)
                     {
                         map[i, j].PrintSymbol();
                     }
