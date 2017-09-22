aea199e03b1ba6f2f7fd65581df7f90917525375
Otto <otto.rinaldo@gmail.com>
2017-09-22 10:05:33 +0200

    moving implementation start

--- Lab4/Program.cs
+++ Lab4/Program.cs
@@ -14,6 +14,48 @@ namespace Lab4
             Door d1 = new Door();
             Console.WriteLine(w1.GetSymbol());
             Console.WriteLine("door"+d1.GetSymbol());
+            Object[] array = new Object[5];
+        }
+        public static void MovingPlayer()
+        {
+            // Moving
+            Object PLayerPosY = ;
+            ConsoleKeyInfo a;
+            a = Console.ReadKey();
+            if (a.Key == ConsoleKey.A)
+            {
+                if (PlayerPosY-1 !=Object. )
+                {
+                    PlayerPosY--;
+                }
+            }
+            else if (a.Key == ConsoleKey.D)
+            {
+                if (y < columns - 2)
+                {
+                    y++;
+                }
+            }
+            else if (a.Key == ConsoleKey.S)
+            {
+                if (x < rows - 2)
+                {
+                    x++;
+                }
+            }
+            else if (a.Key == ConsoleKey.W)
+            {
+                if (x != 1)
+                {
+                    x--;
+                }
+            }
+            else if (a.Key == ConsoleKey.Q)
+            {
+                break;
+            }
+            Console.Clear();
+
         }
     }
 }
